2de31d39c9544c38ebaa71d943fe8ccac641bb30
Arnat Ngawsuwan <annsw.work@gmail.com>
2025-05-31 16:24:16 +0700

    06: categories finalization

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,16 +1,18 @@
 import { SearchInput } from "./search-input";
 import { Categories } from "./categories";
-import { Category } from "@/payload-types";
+import { CustomCategory } from "../types";
 
 interface Props {
-    data: Category[];
+    data: CustomCategory[];
 }
 
 export const SearchFilters = ({ data }: Props) => {
     return (
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-            <SearchInput />
-            <Categories data={data} />
+            <SearchInput data={data} />
+            <div className="hidden lg:block">
+                <Categories data={data} />
+            </div>
         </div>
     )
 }
