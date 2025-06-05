7d4b302ab1be5bef23d727132671b4e9f4b42e86
Arnat Ngawsuwan <annsw.work@gmail.com>
2025-06-06 02:28:11 +0700

    07: trpc integration

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { CategoryDropdown } from "./category-dropdown";
-import { CustomCategory } from "../types";
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ListFilterIcon } from "lucide-react";
 import { CategoriesSidebar } from "./categories-sidebar";
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
 
 interface Props {
-    data: CustomCategory[];
+    data: CategoriesGetManyOutput;
 }
 
 export const Categories = ({ data }: Props) => {
@@ -59,7 +59,6 @@ export const Categories = ({ data }: Props) => {
             <CategoriesSidebar 
                 open={isSidebarOpen}
                 onOpenChange={setIsSidebarOpen}
-                data={data}
             />
             {/* Hidden div to measure all items */}
             <div 
