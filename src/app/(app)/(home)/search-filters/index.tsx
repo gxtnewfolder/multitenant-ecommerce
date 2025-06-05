5451e96c2fa2cd4810ab13ffaf800e1228708e5f
Arnat Ngawsuwan <annsw.work@gmail.com>
2025-06-06 02:40:49 +0700

    07-trpc-integration-fix

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -6,7 +6,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const SearchFilters = () => {
     const trpc = useTRPC();
-    const { data} = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+    const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
     return (
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full" style={{ backgroundColor: '#F5F5F5' }}>
             <SearchInput />
